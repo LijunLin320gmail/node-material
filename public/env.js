11918f332f397313049796192ed3ce7277709b5d
lijun Lin <lijunlin320@gmail.com>
2021-12-05 19:56:03 +1100

    clone.git

--- public/env.js
+++ public/env.js
@@ -1,27 +1,56 @@
-const testButtonFunction=()=>{
-  alert('Thank you for clicking')
+
+// To submit newproject
+const submitProject=(project)=>{
+  $.ajax({
+    url:'/api/projects',
+    contentType:'application/jason',
+    data:JSON.stringify(project),
+    type:'POST',
+    success:function(result){
+      alert('Project Successfully submitted')
+    }
+  });
 }
 
-// connect to the socket
 
-let socket = io();
+const newProject=()=>{
+  let name = $('#name').val()
+  let title = $('#title').val()
+  let video = $('#video').val()
+  let image = $('#image').val()
+  let description = $('#description').val()
+  let project = {name,title,video,image,description}
+  console.log(project)
+  submitProject(project)
+}
 
 
-socket.on('number', (msg) => {
-    console.log('Random number: ' + msg);
-})
 
-console.log('test')
-$(document).ready(function(){
-  console.log('Ready')
+
+const testButtonFunction=()=>{
+    alert('Thank you for clicking')
+  }
   
-  //bind the button
-  $('#testButton').click(testButtonFunction)
-
-  //test get call
-  $.get('/test?user_name="Fantastic User"',(result)=>{
-    console.log(result)
+  // connect to the socket
+  
+  let socket = io();
+  
+  
+  socket.on('number', (msg) => {
+      console.log('Random number: ' + msg);
   })
-
-
-})
+  
+  console.log('test')
+  $(document).ready(function(){
+    console.log('Ready')
+    
+    //bind the button
+    $('#testButton').click(testButtonFunction)
+  
+    //test get call
+    $.get('/test?user_name="Fantastic User"',(result)=>{
+      console.log(result)
+    })
+  
+  
+  })
--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 let express = require("express");
+let dbobjects = require("./db/connection");
 let app = express();
 
 //var app = require('express')();
 let http = require('http').createServer(app);
 let io = require('socket.io')(http);
-
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
 
 
 
@@ -12,6 +14,30 @@ let io = require('socket.io')(http);
 var port = process.env.PORT || 8080;
 
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.jason());
+
+const dummyProject={
+  author:'alessio',
+  imageUrl:'',
+  videoUrl:'',
+  uniqueID:'',
+  description:'choose an apliances to control it.',
+  title:'controller'
+}
+let dummyData=[dummyProject,dummyProject]
+
+//require project data from server
+app.get('/project01',(req,res)=>{
+  console.log('projects requested')
+res.send(dummyData)
+})
+app.post('/project01',(req,res)=>{
+  console.log('projects requested')
+  console.log('body',req.body)
+  let project=req.body;
+  dummyData.push(project)
+  res.send({result:200})
+})
 
 app.get("/test", function (request, response) {
   var user_name = request.query.user_name;
@@ -31,10 +57,50 @@ io.on('connection', (socket) => {
 
 });
 
+dbobjects.connectToDatabase(function (err){
+  if (err){
+    console.error(err);
+    process.exit();
+  }
 
-http.listen(port,()=>{
-  console.log("Listening on port ", port);
+
+
+
+  http.listen(port,()=>{
+    console.log("Listening on port ", port);
+  });
 });
-
 //this is only needed for Cloud foundry 
 require("cf-deployment-tracker-client").track();
+
+
+
+
+//Database connection
+const uri = ""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
